7e6249849cbba0e1b15844f6e52e2a26154047bc
narain <narain@aroliant.com>
2019-02-22 16:10:22 +0530

    Pricing Card Hover Style

---
+++ assets/js/scripts.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+
+    // $('.Pricing__content').bind('click', function () {
+    //     // remove the active class from all elements with active class
+    //     $('.Pricing__cardTitleActive').removeClass('Pricing__cardTitleActive')
+    //     // add active class to clicked element
+    //     $(this).addClass('Pricing__cardTitleActive');
+    //     console.log('clicked');
+
+    // });
+
+
+});
